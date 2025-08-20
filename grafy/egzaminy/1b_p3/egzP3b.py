9220e6b0a50154241dcc9009f7f836f819f791d6
liskowska <malgorzata.liskowska@wp.pl>
2025-08-20 12:55:59 +0200

    egzaminy cd kruskal

--- grafy/egzaminy/1b_p3/egzP3b.py
+++ grafy/egzaminy/1b_p3/egzP3b.py
@@ -1,8 +1,68 @@
-from egzP3btesty import runtests 
+"""
+O(nlogm)
+"""
+
+from egzP3btesty import runtests
 from queue import PriorityQueue
 
 def lufthansa ( G ):
-    #
-    return 0
+    n = len(G)
+    parent = [-1 for _ in range(n + 1)]
 
-runtests(lufthansa, all_tests=False)
+    # zwraca parent/root/set do ktorego nalezy wierzcholek
+    def find(v):
+        if parent[v] < 0:
+            return v
+        parent[v] = find(parent[v])  # path compression
+        return parent[v]
+
+    # False: dodanie v tworzy cykl (nie dodalismy krawedzi)
+    # True: dodanie nie v stworzy cyklu (dodalismy krawedz)
+    def union(v, u):
+        rootv, rootu = find(v), find(u)
+        if rootv == rootu:
+            return False  # union v i u stworzyłby cykl
+        else: #dopinamy do wiekszego roota
+            rank = parent[rootv] + parent[rootu]
+            if parent[rootv] > parent[rootu]: # rootu jest wiekszy
+                parent[rootv] = rootu
+                parent[rootu] = rank
+            else:
+                parent[rootu] = rootv
+                parent[rootv] = rank
+            return True
+
+    def to_edges(G):
+        edges = []
+        total_w = 0
+        for v in range(len(G)):
+            for u, w in G[v]:
+                if u < v: continue
+                edges.append((v, u, w))
+                total_w += w
+        return edges, total_w
+
+
+    #Tworzenie kolejki priorytetowej z priorytetem dla wag
+    queue = PriorityQueue()
+    edges, total_w = to_edges(G)
+    for v, u, w in edges: #nlogm
+        neg_w = -w
+        queue.put((neg_w, v, u)) #-w, zeby ustawily mi sie w kolejce od najwiekszego
+
+    ans = 0
+    use = 0
+    while not queue.empty():
+        neg_w, v, u = queue.get() #logm
+        w = -neg_w
+        if union(v, u):
+            ans += w
+        else:
+            if use == 0:
+                use += 1
+                ans += w
+    # for v1, v2 in G:
+    #     if not union(v1, v2): return [v1, v2]
+    return total_w - ans
+
+runtests(lufthansa, all_tests=True)
---
+++ grafy/kolokwia/2_202324/kol2-wzorcowka.py
@@ -0,0 +1,46 @@
+from kol2testy import runtests
+
+def to_list(G):
+    n = 0
+    for v, u, _w in G:
+        n = max(n, v, u)
+    n += 1
+
+    graph_list = [[] for _ in range(n)]
+    for v, u, w in G:
+        graph_list[v].append((u, w))
+        graph_list[u].append((v, w))
+
+    return graph_list
+
+def graph_modify(G):
+    for v in range(len(G)):
+        G[v].append((v, 8))
+    return G
+
+from collections import deque
+def BFS_status(G, s, status):
+    n = len(G)
+    Q = deque()
+    parent = [None for _ in range(n)]
+    distance = [[float("inf")]*17 for _ in range(n)]
+
+    distance[s][16] = 0
+    Q.append(s)
+
+    while Q:
+        v = Q.popleft()
+        for u in G[v]:
+
+
+    return
+
+def warrior(G, s, t):
+    g1 = to_list(G)
+    graph = graph_modify(g1)
+
+    return -1
+
+
+# zmien all_tests na True zeby uruchomic wszystkie testy
+runtests( warrior, all_tests = True )
